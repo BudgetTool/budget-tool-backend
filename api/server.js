fc67e2eeb0da10e2ed2b079431dc217b8a5e8f7f
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-05-21 20:03:38 -0600

    add mongoose and test messsage

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 
 //api imports
 const express = require("express");
+const db = require('../ds/db.js');
 // brings in express to create the application or server
 const server = express();
 
 //route imports
 const baseUrl = process.env.BASE_URL || "http://localhost:4000/";
+
 // Require routes
 
 //Require middleware
@@ -14,6 +16,14 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 //end imports
+
+// connect to database
+db
+  .connectTo()
+  .then(() => console.log('...SUCCESS: Connected to database...'))
+  .catch(err => console.log('...ERROR: No database connection...', err));
+
+const Message = require("../ds/models/Message");
 
 server.use(express.json());
 server.use(helmet());
@@ -24,7 +34,7 @@ server.use(
     origin: [baseUrl],
     AccessControlAllowOrigin: [
       "http://localhost:4000/",
-        //heruko host route needed here
+      //heruko host route needed here
     ],
     AccessControlAllowHeaders: "Authorization"
   })
@@ -36,6 +46,13 @@ server.use(
 
 server.get("/", (req, res) => {
   res.send("Hello World, from the Budget tool");
+});
+
+server.get("/messages", (req, res) => {
+  console.log('test');
+  Message.find()
+    .then(data => res.json(data))
+    .catch(err => console.error(err))
 });
 
 module.exports = server;
